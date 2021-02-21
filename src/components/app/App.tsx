0a06b9e9e7129952017ee6a2514c1fc7a5ff26ef
black-sheep777 <forough.fallahfar@gmail.com>
2021-02-21 13:33:19 +0330

    Change main pages template

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import logo from 'logo.svg'
+import Header from 'components/header/Header'
 import 'components/app/App.scss'
-import ScalableGrid from 'components/scalable-grid/ScalableGrid'
+
 
 
 
 function App() {
   return (
     <div className="App">
-     
-      <ScalableGrid />
+      <Header />
+      
     </div>
   )
 }
